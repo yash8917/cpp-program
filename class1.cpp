357ff02ef6743633a6e93ae1f53a792a17aa40bd
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2021-12-23 23:07:47 +0530

    Add files via upload

---
+++ class1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class a
+{
+    public:
+    
+    void x()
+    {
+        cout<<"MY NAME IS ANKITA";
+    }
+};
+int main()
+{
+    a ob;
+    ob.x();
+    return 0;
+}
